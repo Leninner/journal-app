b0a547b079de3956bb700c19c52bf7de5fe95126
leninner <mazabandalenin180@gmail.com>
2022-08-04 19:53:47 -0500

    Working on the JournalApp

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,20 @@
+import { Box } from '@mui/system'
+import { Navbar } from '../components'
+
+const drawerWidth = 240
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      {/* Navbar drawerWidth*/}
+      <Navbar drawerWidth={drawerWidth} />
+
+      {/*Sidebar */}
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {/* Toolbar */}
+        {children}
+      </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,14 @@
 import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
-  return <Typography>JournalPage</Typography>
+  return (
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem aperiam, numquam
+        itaque ipsum saepe illo pariatur nesciunt quas facere nulla voluptatibus dolore molestiae
+        odio dolor repellat praesentium hic, eos quibusdam.
+      </Typography>
+    </JournalLayout>
+  )
 }

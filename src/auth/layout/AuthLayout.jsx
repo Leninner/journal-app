4d676ee39ae5cf74c5459a8ea8b40ed5d20febae
leninner <mazabandalenin180@gmail.com>
2022-08-15 16:20:45 -0500

    Updated linters and formaters

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -1,6 +1,11 @@
 import { Grid, Typography } from '@mui/material'
 
 export const AuthLayout = ({ children, title = '' }) => {
+  const doSome = () => console.log('Leninsin')
+
+  const a = 10
+  const b = 20
+
   return (
     <Grid
       container
@@ -14,7 +19,12 @@ export const AuthLayout = ({ children, title = '' }) => {
         item
         className="box-shadow"
         xs={3}
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2, width: { md: 450 } }}
+        sx={{
+          backgroundColor: 'white',
+          padding: 3,
+          borderRadius: 2,
+          width: { md: 450 },
+        }}
       >
         <Typography variant="h5" sx={{ mb: 1 }}>
           {title}
